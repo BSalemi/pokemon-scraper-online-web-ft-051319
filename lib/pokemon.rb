cddd24ead079576be32b8e7eb07b5caff2e2eda4
Brian Salemi <briandsalemi@gmail.com>
2019-06-15 15:51:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,10 +13,10 @@ class Pokemon
 
   def self.save(name, type, db)
     sql = <<-SQL
-    INSERT INTO pokemon(name, type) VALUES(?,?)
+    INSERT INTO pokemon(name, type)
+    VALUES(?,?)
     SQL
     db.execute(sql, name, type)
-
     @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
 
